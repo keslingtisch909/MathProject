6966ff33394ccead8859d56f59ed7c56e61d9c93
keslingtisch909 <keslingtisch909@gmail.com>
2025-03-30 08:07:20 +0000

    Add 20.cpp

---
+++ src/20.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    // Add operation
+    std::cout << "The sum of " << a << " and " << b << " is: " << (a + b) << std::endl;
+
+    return 0;
+}
