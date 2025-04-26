739553d77bc9c3e95cabe9d3e1d590dd64615fad
keslingtisch909 <keslingtisch909@gmail.com>
2025-04-26 02:07:10 +0000

    Add 33.cpp

---
+++ src/33.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    float result = a / b; // Division of two integers
+    cout << "The division is: " << result << endl; // Output the result to the console
+
+    return 0;
+}
