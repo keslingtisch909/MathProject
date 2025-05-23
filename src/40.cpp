d452a22d113de9d57921a6ca3bc07cd694caf8dc
keslingtisch909 <keslingtisch909@gmail.com>
2025-05-23 05:15:38 +0000

    Add 40.cpp

---
+++ src/40.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Example code snippet
+    int x = 10;
+    int y = 5;
+
+    std::cout << "The sum of " << x << " and " << y << " is: " << x + y << std::endl;
+    return 0;
+}
